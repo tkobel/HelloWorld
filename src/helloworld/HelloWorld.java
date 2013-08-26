46c3cb11a9ec8260fce2ae1eabf50e5e7e78c2f7
Tracy <Tracy@10.30.17.18>
2013-08-26 17:58:34 -0500

    Revert "changed main method"
    This reverts commit 0e0a57ba6654b1999e3d070351452d8a00af01cc.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -14,6 +14,6 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // TODO code application logic here
     }
 }
